15f1260bca550ad792384df44bc02185e7e2adba
ReenAG <heeboy007@naver.com>
2024-04-26 17:53:10 +0900

    platform: android and ios confirmed

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
  * @format
  */
 import 'react-native-gesture-handler';
+import "react-native-url-polyfill/auto";
 import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+//simulator register...
 AppRegistry.registerComponent('main', () => App);
+
+//normal register...
 AppRegistry.registerComponent(appName, () => App);
 
 if(Platform.OS == 'web') {
